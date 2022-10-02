1c442045e1a8570b736d851d672a48f69935af06
Orien Madgwick <vcs@orien.io>
2022-10-02 15:03:08 +1100

    Remove spell completion

--- configuration/vim/.config/nvim/lua/languages.lua
+++ configuration/vim/.config/nvim/lua/languages.lua
@@ -1,13 +1,11 @@
 local null_ls = require('null-ls')
 local code_actions = null_ls.builtins.code_actions
-local completion = null_ls.builtins.completion
 local diagnostics = null_ls.builtins.diagnostics
 local hover = null_ls.builtins.hover
 null_ls.setup({
   sources = {
     code_actions.proselint,
     diagnostics.proselint,
-    completion.spell,
     hover.dictionary,
 
     code_actions.gitsigns,
