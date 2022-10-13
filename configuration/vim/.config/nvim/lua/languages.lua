cee6e56a6fcb7c562e0207351ed7d67c05f0b5da
Orien Madgwick <vcs@orien.io>
2022-10-13 21:16:24 +1100

    Fix Neovim treesitter highlighting

--- configuration/vim/.config/nvim/lua/languages.lua
+++ configuration/vim/.config/nvim/lua/languages.lua
@@ -135,7 +135,7 @@ require('nvim-treesitter.configs').setup({
   sync_install = false,
   auto_install = true,
   highlight = {
-    enabled = true,
+    enable = true,
   },
   incremental_selection = {
     enable = true,
