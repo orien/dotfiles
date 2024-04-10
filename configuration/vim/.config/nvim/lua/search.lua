17b3470824ac66ff4b4d9a75a497c5d24edf214b
Orien Madgwick <497874+orien@users.noreply.github.com>
2024-04-10 23:09:13 +1000

    Use AstroNvim template

--- configuration/vim/.config/nvim/lua/search.lua
+++
@@ -1,6 +0,0 @@
-
-local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>sb', builtin.buffers, {})
-vim.keymap.set('n', '<leader>sf', builtin.find_files, {})
-vim.keymap.set('n', '<leader>sh', builtin.help_tags, {})
-vim.keymap.set('n', '<leader>sp', builtin.live_grep, {})
