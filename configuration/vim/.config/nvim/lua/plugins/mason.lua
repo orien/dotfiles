0cb486cd17e693a920c32e3d8aec7a48fa26de6b
Orien Madgwick <vcs@orien.io>
2024-04-11 19:08:40 +1000

    Use the Ruby language server instead of solargraph

--- configuration/vim/.config/nvim/lua/plugins/mason.lua
+++ configuration/vim/.config/nvim/lua/plugins/mason.lua
@@ -8,9 +8,7 @@ return {
     -- overrides `require("mason-lspconfig").setup(...)`
     opts = function(_, opts)
       -- add more things to the ensure_installed table protecting against community packs modifying it
-      opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, {
-        "rubocop",
-      })
+      opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, {})
     end,
   },
   -- use mason-null-ls to configure Formatters/Linter installation for null-ls sources
