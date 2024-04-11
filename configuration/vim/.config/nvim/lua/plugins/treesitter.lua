24112b8c8fdd0c3c1a5bf186b18a7d0aec5846e4
Orien Madgwick <497874+orien@users.noreply.github.com>
2024-04-11 14:51:38 +1000

    Install community-supported plugin configuration a variety of languages

--- configuration/vim/.config/nvim/lua/plugins/treesitter.lua
+++ configuration/vim/.config/nvim/lua/plugins/treesitter.lua
@@ -6,19 +6,9 @@ return {
   opts = function(_, opts)
     -- add more things to the ensure_installed table protecting against community packs modifying it
     opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, {
-      "css",
       "gitcommit",
       "gitignore",
-      "go",
-      "javascript",
-      "lua",
-      "ruby",
-      "rust",
-      "scss",
-      "sql",
-      "typescript",
       "vim",
-      "vue",
     })
   end,
 }
