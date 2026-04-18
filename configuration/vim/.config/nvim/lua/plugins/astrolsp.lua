a4f4eca0ddc0fed7be54ae8f83d5734243b020bb
Orien Madgwick <vcs@orien.io>
2026-04-18 13:32:54 +1000

    Nvim: add Pony language support (ponylsp + treesitter)

--- configuration/vim/.config/nvim/lua/plugins/astrolsp.lua
+++ configuration/vim/.config/nvim/lua/plugins/astrolsp.lua
@@ -38,7 +38,7 @@ return {
     },
     -- enable servers that you already have installed without mason
     servers = {
-      -- "pyright"
+      "ponylsp",
     },
     -- customize language server configuration options passed to `lspconfig`
     ---@diagnostic disable: missing-fields
