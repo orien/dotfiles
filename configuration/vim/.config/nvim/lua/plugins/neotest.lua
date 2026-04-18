382484b03ebf7581bfbac4b82a57cd389890428c
Orien Madgwick <vcs@orien.io>
2026-04-18 12:43:04 +1000

    Nvim: remove dead opts.ft from neotest config
    opts.ft inside an opts function is passed to neotest.setup(), not the
    lazy.nvim spec — neotest has no ft option so it was silently ignored.

--- configuration/vim/.config/nvim/lua/plugins/neotest.lua
+++ configuration/vim/.config/nvim/lua/plugins/neotest.lua
@@ -8,9 +8,6 @@ return {
       "olimorris/neotest-rspec",
     },
     opts = function(_, opts)
-      opts.ft = require("astrocore").list_insert_unique(opts.ft, {
-        "ruby",
-      })
       opts.adapters = require("astrocore").list_insert_unique(opts.adapters, {
         require "neotest-rspec",
       })
